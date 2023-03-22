5d698ea39623e22a0a6027d76ed03589630c3b57
George M <gmeyer1000@yahoo.com>
2023-03-22 15:30:41 -0700

    allowed host

--- nextdrf/settings.py
+++ nextdrf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ef7j%)j=3&v)=y8!%7kpnn^--0rwe=1ll!$5013s(ztwde2wp3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://applestore23.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://applestore23.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
